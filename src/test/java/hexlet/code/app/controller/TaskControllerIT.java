f271bbdd073bfe32e75fabd330417733372f8295
mike <korobov.mike@gmail.com>
2023-04-17 19:52:32 +0500

    Fix linter errors-2

--- src/test/java/hexlet/code/app/controller/TaskControllerIT.java
+++ src/test/java/hexlet/code/app/controller/TaskControllerIT.java
@@ -1,10 +1,8 @@
 package hexlet.code.app.controller;
 
 
-import com.fasterxml.jackson.core.type.TypeReference;
+
 import hexlet.code.app.config.SpringConfigForIT;
-import hexlet.code.app.dto.TaskDto;
-import hexlet.code.app.model.Task;
 import hexlet.code.app.repository.LabelRepository;
 import hexlet.code.app.repository.TaskRepository;
 import hexlet.code.app.utils.TestUtils;
@@ -18,20 +16,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.HashSet;
-import java.util.Set;
+//import com.fasterxml.jackson.core.type.TypeReference;
+//import hexlet.code.app.dto.TaskDto;
+//import hexlet.code.app.model.Task;
+//import java.util.HashSet;
+//import java.util.Set;
 
+//import static org.springframework.http.MediaType.APPLICATION_JSON;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import static hexlet.code.app.controller.TaskController.TASK_CONTROLLER_PATH;
+//import static hexlet.code.app.utils.TestUtils.asJson;
+//import static hexlet.code.app.utils.TestUtils.fromJson;
+//import static hexlet.code.app.utils.TestUtils.TEST_EMAIL;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static hexlet.code.app.config.SpringConfigForIT.TEST_PROFILE;
-import static hexlet.code.app.controller.TaskController.TASK_CONTROLLER_PATH;
-import static hexlet.code.app.utils.TestUtils.asJson;
-import static hexlet.code.app.utils.TestUtils.fromJson;
-import static hexlet.code.app.utils.TestUtils.TEST_EMAIL;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @AutoConfigureMockMvc
 @ActiveProfiles(TEST_PROFILE)
