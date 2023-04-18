6aa2501c32875cd8454021f6848dc96a98745e60
mike <korobov.mike@gmail.com>
2023-04-19 01:08:54 +0500

    Fix github tests-2

--- src/main/java/hexlet/code/config/security/SecurityConfig.java
+++ src/main/java/hexlet/code/config/security/SecurityConfig.java
@@ -92,8 +92,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().disable()
                 .httpBasic().disable()
                 .logout().disable();
-
-        
     }
 
 }
